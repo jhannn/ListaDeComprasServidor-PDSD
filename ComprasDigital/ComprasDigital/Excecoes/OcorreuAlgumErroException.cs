ba65bb71b077da5e149aebb731c8d1775ebd77be
jhannn <johann.guerra200@gmail.com>
2015-01-21 16:38:23 -0200

    Commit 0.3
    Faltava o OcorreuAlgumErroException no commit anterior!!

---
+++ ComprasDigital/ComprasDigital/Excecoes/OcorreuAlgumErroException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComprasDigital.Excecoes
+{
+	public class OcorreuAlgumErroException : Exception
+	{
+		public string erro { get; set; }
+		public OcorreuAlgumErroException()
+			: base("Ocorreu algum erro! Por favor repita o procedimento!")
+		{
+			erro = "Erro de Usuário";
+		}
+	}
+}
