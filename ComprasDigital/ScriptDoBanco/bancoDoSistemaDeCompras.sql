b5d666a44e525573153fd2ec659257b82f2254dc
Marcos <marcos.antas@hotmail.com.br>
2015-02-03 08:46:38 -0300

    Alterações no banco
    Essas alterações não deverão ser feitas agora

--- ComprasDigital/ScriptDoBanco/bancoDoSistemaDeCompras.sql
+++ ComprasDigital/ScriptDoBanco/bancoDoSistemaDeCompras.sql
@@ -17,61 +17,61 @@ CREATE TABLE tb_Usuario
 CREATE TABLE tb_Marca
 (
 	id_marca INT PRIMARY KEY IDENTITY(1,1),
-	marca VARCHAR(50)
+	marca VARCHAR(25)
 );
 
 
 
 CREATE TABLE tb_Tipo
 (
-	id_tipo INT PRIMARY KEY IDENTITY(1,1),
-	tipo varchar(50)
+	id_tipo INT PRIMARY KEY,
+	tipo varchar(25)
 );
-INSERT INTO tb_Tipo VALUES ('Outro');
-INSERT INTO tb_Tipo VALUES ('Combustivel');
-INSERT INTO tb_Tipo VALUES ('Futa, Legume ou Verdura');
-INSERT INTO tb_Tipo VALUES ('Eletrônico');
+INSERT INTO tb_Tipo VALUES (1, 'Outro');
+INSERT INTO tb_Tipo VALUES (2, 'Combustivel');
+INSERT INTO tb_Tipo VALUES (3, 'Fruta, Legume ou Verdura');
+INSERT INTO tb_Tipo VALUES (4, 'Eletrônico');
 --Adicionar outros
 
 
 
 CREATE TABLE tb_Unidade
 (
-	id_unidade INT PRIMARY KEY IDENTITY(1,1),
-	unidade varchar(50)
+	id_unidade INT PRIMARY KEY,
+	unidade varchar(15)
 );
-INSERT INTO tb_Unidade VALUES ('Unidade');
-INSERT INTO tb_Unidade VALUES ('KG');
-INSERT INTO tb_Unidade VALUES ('Gramas');
-INSERT INTO tb_Unidade VALUES ('Litro');
+INSERT INTO tb_Unidade VALUES (1, 'Unidade');
+INSERT INTO tb_Unidade VALUES (2, 'KG');
+INSERT INTO tb_Unidade VALUES (3, 'Gramas');
+INSERT INTO tb_Unidade VALUES (4, 'Litro');
 
 
 
 CREATE TABLE tb_Embalagem
 (
-	id_embalagem INT PRIMARY KEY IDENTITY(1,1),
-	embalagem varchar(20)
+	id_embalagem INT PRIMARY KEY,
+	embalagem varchar(15)
 );
-INSERT INTO tb_Embalagem VALUES ('Outra');
-INSERT INTO tb_Embalagem VALUES ('Unidade');
-INSERT INTO tb_Embalagem VALUES ('Pacote');
-INSERT INTO tb_Embalagem VALUES ('Caixa');
-INSERT INTO tb_Embalagem VALUES ('Garrafa');
-INSERT INTO tb_Embalagem VALUES ('Lata');
-INSERT INTO tb_Embalagem VALUES ('Barra');
-INSERT INTO tb_Embalagem VALUES ('Peso');
+INSERT INTO tb_Embalagem VALUES (1, 'Outra');
+INSERT INTO tb_Embalagem VALUES (2, 'Unidade');
+INSERT INTO tb_Embalagem VALUES (3, 'Pacote');
+INSERT INTO tb_Embalagem VALUES (4, 'Caixa');
+INSERT INTO tb_Embalagem VALUES (5, 'Garrafa');
+INSERT INTO tb_Embalagem VALUES (6, 'Lata');
+INSERT INTO tb_Embalagem VALUES (7, 'Barra');
+INSERT INTO tb_Embalagem VALUES (8, 'Peso');
 
 
 
 CREATE TABLE tb_Ocorrencia
 (
-	id_ocorrencia INT PRIMARY KEY IDENTITY(1,1),
-	ocorrencia varchar(50)
+	id_ocorrencia INT PRIMARY KEY,
+	ocorrencia varchar(30)
 );
-INSERT INTO tb_Ocorrencia VALUES ('Codigo de barras ja existente');
-INSERT INTO tb_Ocorrencia VALUES ('Codigo de barras diferente do existente');
-INSERT INTO tb_Ocorrencia VALUES ('Tipo diferente');
-INSERT INTO tb_Ocorrencia VALUES ('Unidade diferente');
+INSERT INTO tb_Ocorrencia VALUES (1, 'Codigo de barras ja existente');
+INSERT INTO tb_Ocorrencia VALUES (2, 'Codigo de barras diferente');
+INSERT INTO tb_Ocorrencia VALUES (3, 'Tipo diferente');
+INSERT INTO tb_Ocorrencia VALUES (4, 'Unidade diferente');
 
 
 
@@ -89,7 +89,7 @@ CREATE TABLE tb_Estabelecimento
 CREATE TABLE tb_Produto
 (
 	id_produto INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50) NOT NULL, 
+	nome VARCHAR(40) NOT NULL, 
 	marca INT FOREIGN KEY REFERENCES tb_Marca(id_marca) NOT NULL,
 	codigoDeBarras VARCHAR(50),
 	tipoCodigoDeBarras VARCHAR(50),
@@ -102,7 +102,7 @@ CREATE TABLE tb_Produto
 CREATE TABLE tb_ListaDeProdutos
 (
 	id_listaDeProdutos INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
+	nome VARCHAR(33),
 	id_usuario INT FOREIGN KEY REFERENCES tb_Usuario(id_usuario) NOT NULL
 );
 
@@ -152,9 +152,74 @@ CREATE TABLE tb_ListaDeItens
 CREATE TABLE tb_ItemDaLista
 (
 	id_lista INT FOREIGN KEY REFERENCES tb_ListaDeItens(id_listaDeItens) NOT NULL,
-	nome_produto VARCHAR(50) NOT NULL,
-	marca_produto VARCHAR(50) NOT NULL,
+	nome_produto VARCHAR(40) NOT NULL,
+	marca_produto VARCHAR(25) NOT NULL,
 	preco FLOAT NOT NULL,
 	unidade INT FOREIGN KEY REFERENCES tb_Unidade(id_unidade) NOT NULL,
 	quantidade INT NOT NULL,
 );
+
+
+
+
+
+/*			#----------------------------------------------#			*/
+/*			|     _____   _____   _____   ___     _____    |			*/
+/*			|    /       |       |       |   \   /         |			*/
+/*			|    \____   |____   |____   |    |  \____     |			*/
+/*			|         \  |       |       |    |       \    |			*/
+/*			|    _____/  |_____  |_____  |___/   _____/    |			*/
+/*			|											   |			*/
+/*			#----------------------------------------------#			*/
+
+
+
+
+
+INSERT INTO tb_Marca VALUES ('');
+INSERT INTO tb_Marca VALUES ('garoto');
+INSERT INTO tb_Marca VALUES ('nestle');
+INSERT INTO tb_Marca VALUES ('parmalat');
+
+INSERT INTO tb_Estabelecimento VALUES ('Nordestão', 'ZN', 'Natal', 0);
+INSERT INTO tb_Estabelecimento VALUES ('Carrefour', 'ZN', 'Natal', 0);
+INSERT INTO tb_Estabelecimento VALUES ('Nordestão', 'ZS', 'Natal', 0);
+
+INSERT INTO tb_Produto VALUES('banana',@vazio , null, null, 2, @Outra);
+INSERT INTO tb_Produto VALUES('maçã',@vazio , null, null, 2, @Outra);
+INSERT INTO tb_Produto VALUES('gasolina',@vazio , null, null, 4, @Outra);
+INSERT INTO tb_Produto VALUES('alcool',@vazio , null, null, 4, @Outra);
+INSERT INTO tb_Produto VALUES('alcool',@vazio , null, null, 1, @Garrafa);
+INSERT INTO tb_Produto VALUES('variedades',@nestle , null, null, 1, @Caixa);
+INSERT INTO tb_Produto VALUES('neston 200ml',@nestle , null, null, 1, @Garrafa);
+INSERT INTO tb_Produto VALUES('nescau 200ml',@nestle , null, null, 1, @Garrafa);
+INSERT INTO tb_Produto VALUES('alpino 200ml',@nestle , null, null, 1, @Garrafa);
+INSERT INTO tb_Produto VALUES('farinha lactea 400g',@nestle , null, null, 1, @Lata);
+INSERT INTO tb_Produto VALUES('baton',@garoto , null, null, 1, @Unidade);
+INSERT INTO tb_Produto VALUES('talento 100g',@garoto , null, null, 1, @Barra);
+INSERT INTO tb_Produto VALUES('talento',@garoto , null, null, 1, @Unidade);
+INSERT INTO tb_Produto VALUES('castanha de caju',@garoto , null, null, 1, @Barra);
+INSERT INTO tb_Produto VALUES('leite integral',@parmalat , null, null, 1, @Caixa);
+INSERT INTO tb_Produto VALUES('milk',@parmalat , null, null, 1, @Caixa);
+INSERT INTO tb_Produto VALUES('classico semidesnatado',@parmalat , null, null, 1, @Caixa);
+
+
+
+SELECT id_unidade AS id, unidade
+FROM tb_Unidade;
+
+SELECT id_marca AS id, marca
+FROM tb_Marca;
+
+SELECT id_embalagem AS id, embalagem
+FROM tb_Embalagem
+
+SELECT id_estabelecimento AS id, nome, bairro, cidade, numero
+FROM tb_Estabelecimento;
+
+SELECT p.id_produto AS id, m.marca, p.nome, e.embalagem, u.unidade, p.codigoDeBarras, p.tipoCodigoDeBarras
+FROM tb_Produto AS p
+INNER JOIN tb_Marca AS m ON p.marca = m.id_marca
+INNER JOIN tb_Unidade AS u ON p.unidade = u.id_unidade
+INNER JOIN tb_Embalagem AS e ON p.embalagem = e.id_embalagem
+ORDER BY m.marca, p.nome, e.embalagem, u.unidade
