e81d7dff1811fe89f9b540683380c41a78ac7917
Marcos <marcos.antas@hotmail.com.br>
2015-03-05 16:11:39 -0300

    Deserialização iniciada

---
+++ ComprasDigital/ComprasDigital/Classes/jsListaDeItens.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComprasDigital.Classes
+{
+	public class jsListaDeItens
+	{
+		public int idEstabelecimento { get; set; }
+		public DateTime dataDeCompras { get; set; }
+		public int idUsuario { get; set; }
+		public jsItem[] itensComprados { get; set; }
+	}
+}
--- ComprasDigital/ComprasDigital/Servidor/ListaDeItens.asmx.cs
+++ ComprasDigital/ComprasDigital/Servidor/ListaDeItens.asmx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.Services;
 using ComprasDigital.Classes;
+using ComprasDigital.Excecoes.Usuario;
 
 namespace ComprasDigital.Servidor
 {
@@ -19,9 +21,23 @@ namespace ComprasDigital.Servidor
     {
 
         [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hello World";
+		public string finalizarCheckin(int idUsuario, string token, string lista)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();//O JavaScriptSerializer vai fazer o web service retornar JSON
+
+			//if (!cUsuario.usuarioValido(idUsuario, token))
+			//	return js.Serialize(new UsuarioNaoLogadoException()); //retorna a exception UsuarioNaoLogado
+
+			/*/	Exemplo de entrada:
+			 *	{"idEstabelecimento":1,"dataDeCompras":"\/Date(1425524400000)\/","idUsuario":1,"itensComprados":
+			 *	[
+			 *		{"idProduto":1,"nome":"Nescau","marca":"Nestle","codigoDeBarras":null,"tipoCodigo":null,"embalagem":1,"unidade":0,"quantidade":2,"preco":2.5},
+			 *		{"idProduto":1,"nome":"milk","marca":"parmalat","codigoDeBarras":null,"tipoCodigo":null,"embalagem":2,"unidade":0,"quantidade":1,"preco":3}
+			 *	]}
+			/*/
+			jsListaDeItens listaDeItens = js.Deserialize<jsListaDeItens>(lista);
+
+			return js.Serialize(listaDeItens);
         }
     }
 }
